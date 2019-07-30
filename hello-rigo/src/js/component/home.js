3e49c1871d2d461b62d8fe54a0a90e2a8fbb4370
Luis Javier Rodriguez Ortega <ljavierrodriguez@gmail.com>
2019-07-30 00:42:28 +0000

    Add Component Footer

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 
 import { Navbar } from "./navbar";
+import { Footer } from "./footer";
 
 export function Home(props) {
 	return (
 		<Fragment>
 			<Navbar />
 			<h1>Home</h1>
+			<Footer />
 		</Fragment>
 	);
 }
