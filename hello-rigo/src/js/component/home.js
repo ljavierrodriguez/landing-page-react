ff8bc6e9a274f5fc52154e52ade99fbf2af8fe0e
Luis Javier Rodriguez Ortega <ljavierrodriguez@gmail.com>
2019-07-30 00:26:55 +0000

    Componente Home y Navbar

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -1,26 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar";
 
-//create your first component
-export class Home extends React.Component {
-	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
-			</div>
-		);
-	}
+export function Home(props) {
+	return (
+		<Fragment>
+			<Navbar />
+			<h1>Home</h1>
+		</Fragment>
+	);
 }
---
+++ hello-rigo/src/js/component/navbar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Navbar(props) {
+	return <h1>Navbar</h1>;
+}
